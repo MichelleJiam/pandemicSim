a0fab6a05d207f3925d37138505de57c3d5184e2
Michelle Jiam <mjiam@student.codam.nl>
2021-12-03 19:39:56 +0100

    Added string test and concat demo to lab

--- src/com/codam/javalab/Test.java
+++ src/com/codam/javalab/Test.java
@@ -14,15 +14,31 @@ class IntegerTest {
     }
 }
 
+class StringTest {
+    String str = "";
+
+    void    addToString(String s2) { str = str.concat(s2); }
+    void    printString() { System.out.println("str is: " + str); }
+}
+
 public class Test {
     public static void main(String[] args) {
         IntegerTest intTest = new IntegerTest();
 
-        if (args.length > 0) {
-            intTest.convertString(args[0]);
+        if (args.length > 1 && args[0].contains("i")) {
+            intTest.convertString(args[1]);
             intTest.printNum();
             intTest.incrementNum(5);
             intTest.printNum();
+        }
+
+        StringTest  strTest = new StringTest();
+
+        if (args.length > 1 && args[0].contains("s")) {
+            strTest.addToString("Hello");
+            strTest.printString();
+            strTest.addToString(" " + args[1]);
+            strTest.printString();
         }
     }
 }
